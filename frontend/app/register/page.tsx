6158a095434b127154de9cc06cc392e0ecc09227
Md Misrat Hossain <mdmishrat13@gmail.com>
2023-11-14 19:28:00 +0600

    backend created

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,49 +1,127 @@
-import React from 'react'
+'use client'
+import Image from "next/image";
+import React, { useState } from "react";
+import { RxAvatar } from "react-icons/rx";
+
 
 const page = () => {
+  const [avatar,setAvatar] = useState('')
+  const [visible,setVisible] = useState(false)
   return (
-   <section className="bg-gray-50 flex flex-col items-center justify-center min-h-screen w-full">
-  <div className="px-6 py-8 mx-auto lg:py-0">
-    <div className="bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0">
-      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-          Create a new account
-        </h1>
-        <form className="space-y-4 md:space-y-6" action="#">
-        <div>
-            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Your Name</label>
-            <input type="text" name="naem" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="Your Name" required/>
-          </div>
-          <div>
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@company.com" required />
-          </div>
-          <div>
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700" required />
-          </div>
-          <div className="flex items-center justify-between">
-            <div className="flex items-start">
-              <div className="flex items-center h-5">
-                <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required />
+    <section className="bg-gray-50 flex flex-col items-center justify-center min-h-screen w-full">
+      <div className="px-6 py-8 mx-auto lg:py-0">
+        <div className="bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+              Create a new account
+            </h1>
+            <form className="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  name="naem"
+                  id="name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  placeholder="Your Name"
+                  required
+                />
               </div>
-              <div className="ml-3 text-sm">
-                <label htmlFor="remember" className="text-gray-500">Remember me</label>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  placeholder="name@company.com"
+                  required
+                />
               </div>
-            </div>
-            <a href="#" className="text-sm font-medium text-primary-600 hover:underline">Forgot password?</a>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700"
+                  required
+                />
+              </div>
+              <div className="flex items-center">
+                <span className="inline-block w-8 h-8 overflow-hidden rounded-full">
+                  {avatar.length?(<Image src={avatar} alt="avatar" width={12} height={12}/>
+                  ):(<RxAvatar className="w-8 h-8 rounded-full" />)}
+                </span>
+                <label
+                  htmlFor="profileImg"
+                  className="ml-5 flex items-center justify-center border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                >
+                  <span className="py-2 px-4 cursor-pointer">Upload an image</span>
+                  <input type="file" name="password" id="profileImg" className="sr-only" required />
+                </label>
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                      required
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label htmlFor="remember" className="text-gray-500">
+                      Remember me
+                    </label>
+                  </div>
+                </div>
+                <a
+                  href="#"
+                  className="text-sm font-medium text-primary-600 hover:underline"
+                >
+                  Forgot password?
+                </a>
+              </div>
+              <button
+                type="submit"
+                className="w-full font-medium text-white bg-gray-900 rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Sign in
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Already have an account?{" "}
+                <a
+                  href="#"
+                  className="font-medium text-primary-600 hover:underline"
+                >
+                  Sign in
+                </a>
+              </p>
+            </form>
           </div>
-          <button type="submit" className="w-full font-medium text-white bg-gray-900 rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
-          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-            Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline">Sign in</a>
-          </p>
-        </form>
+        </div>
       </div>
-    </div>
-  </div>
-</section>
+    </section>
+  );
+};
 
-  )
-}
-
-export default page
+export default page;
